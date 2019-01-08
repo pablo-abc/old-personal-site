896889015d607c36f20f280b0e97bf342bc8043d
Pablo Berganza <pberganza10@gmail.com>
2019-01-08 01:06:45 -0600

    :sparkles: Fetch about content from api

--- src/cljs/berganzapablo/views/about.cljs
+++ src/cljs/berganzapablo/views/about.cljs
@@ -1,6 +1,20 @@
-(ns berganzapablo.views.about)
+(ns berganzapablo.views.about
+  (:require-macros [cljs.core.async :refer [go]])
+  (:require [reagent.core :as reagent]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
 
 
 (defn about-page []
-  (fn [] [:span.main
-          [:h1 "About berganzapablo"]]))
+  (let [state (reagent/atom {:text ""})]
+    (reagent/create-class
+     {:component-did-mount
+      (fn []
+        (go (let [response (<! (http/get "/api/about"
+                                         {:with-credentials? false}))]
+              (case (:status response)
+                200 (reset! state (:body response))))))
+      :reagent-render
+      (fn [] [:span.main
+              [:h1 "About me"]
+              [:p (:text @state)]])})))
