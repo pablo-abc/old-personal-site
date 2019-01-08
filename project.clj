26b0fffb0f87088d06e2f5505097f4c2b2874559
Pablo Berganza <pberganza10@gmail.com>
2019-01-07 22:04:31 -0600

    :heavy_plus_sign: Add cheshire and cljs-http

--- project.clj
+++ project.clj
@@ -17,7 +17,9 @@
                  [metosin/reitit "0.2.10"]
                  [pez/clerk "1.0.0"]
                  [venantius/accountant "0.2.4"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [cljs-http "0.1.45"]
+                 [cheshire "5.8.1"]]
 
   :plugins [[lein-environ "1.1.0"]
             [lein-cljsbuild "1.1.7"]
