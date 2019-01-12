26239ec09cba121ed47b08a9361788e18422a421
Pablo Berganza <pberganza10@gmail.com>
2019-01-12 00:05:00 -0600

    :sparkles: Fetch blog posts from db

--- src/clj/berganzapablo/db/blogs.clj
+++ src/clj/berganzapablo/db/blogs.clj
@@ -1,14 +1,19 @@
-(ns berganzapablo.db.blogs)
+(ns berganzapablo.db.blogs
+  (:require [clojure.java.jdbc :as jdbc]
+            [honeysql.core :as sql]
+            [honeysql.helpers :refer :all :as helpers]
+            [berganzapablo.db.config.db :refer [pg-db]]))
 
-(defn placeholder-blogs []
-  (map (fn [num]
-         {:id num
-          :title (str "Title " num)
-          :introduction (str "Introduction of " num)})
-       (range 1 11)))
+(defn find-query []
+  (-> (select :*)
+     (from :blog_post)))
 
 (defn blogs-find
   "Retrieve blog list from database."
   ([] (blogs-find {}))
   ([filter]
-   (placeholder-blogs)))
+   (jdbc/query pg-db (sql/format (find-query)))))
+
+(def test-query  (-> (select :*)
+                    (from :blog_post)
+                    (where [:like :title "%ost%"])))
--- src/clj/berganzapablo/db/config/db.clj
+++ src/clj/berganzapablo/db/config/db.clj
@@ -1,13 +1,16 @@
 (ns berganzapablo.db.config.db
-  (:require [clojure.java.jdbc :as jdbc]
-            [config.core :refer [env]]
-            [honeysql.core :as sql]
-            [honeysql.helpers :refer :all :as helpers]))
+  (:require [config.core :refer [env]]
+            [honeysql.format :as fmt]))
+
+(defmethod fmt/fn-handler "like" [_ field value]
+  (str (fmt/to-sql field) " LIKE "
+       (fmt/to-sql value)))
+
+(defmethod fmt/fn-handler "ilike" [_ field value]
+  (str (fmt/to-sql field) " ILIKE "
+       (fmt/to-sql value)))
 
 (def pg-db {:dbtype "postgresql"
             :dbname (env :db-database)
             :user (env :db-user)
             :password (env :db-password)})
-
-(jdbc/query pg-db (sql/format (-> (select :*)
-                                 (from :author))))
