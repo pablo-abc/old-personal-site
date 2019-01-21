8a2b075eac374876aa4fbe8b1fa7e753aaab7b43
Pablo Berganza <pberganza10@gmail.com>
2019-01-21 01:32:13 -0600

    :arrow_up: Upgrade cider nrepl

--- project.clj
+++ project.clj
@@ -108,7 +108,7 @@
                            :source-paths ["env/dev/clj"]
                            :plugins
                            [[lein-figwheel "0.5.18"]
-                            [cider/cider-nrepl "0.19.0"]
+                            [cider/cider-nrepl "0.20.0"]
                             [org.clojure/tools.namespace "0.3.0-alpha4"
                              :exclusions [org.clojure/tools.reader]]
                             [refactor-nrepl "2.4.0"
