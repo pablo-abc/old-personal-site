7c5f14f7e6cb38c80f8ca7c293261774fb31b168
Pablo Berganza <pberganza10@gmail.com>
2019-03-07 22:43:07 -0600

    :white_check_mark: Add tests

---
+++ test/clj/berganzapablo/handler_test.clj
@@ -0,0 +1,22 @@
+(ns berganzapablo.handler-test
+  (:use midje.sweet)
+  (:require [berganzapablo.handler :as handler])
+  (:import [java.io ByteArrayInputStream]))
+
+(fact "head returns an array"
+      (handler/head) => #(= (count %) 7)
+      (handler/head) => (has-prefix [:head]))
+
+(fact "mount-layout should return array"
+      (handler/mount-layout [:div "hi"]) => (has-prefix [:div#app]))
+
+(fact "current-page should return a html"
+      (handler/current-page {:uri "/"}) => #"DOCTYPE html")
+
+(fact "index-handler should return a map with an html body"
+      (handler/index-handler {:uri "/"}) => (contains
+                                            {:status 200
+                                             :body #"DOCTYPE html"}))
+
+(fact "keywordize-map should return a map with keywords instead of strings"
+      (handler/keywordize-map {"key" "value"}) => {:key "value"})
